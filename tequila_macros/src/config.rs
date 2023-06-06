74f34e004b9ae3ab8803c232e25be7a112cb3e43
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-06-06 23:41:19 +0200

    add tequila config fetching

---
+++ tequila_macros/src/config.rs
@@ -0,0 +1,63 @@
+use std::str::FromStr;
+
+use reqwest::Url;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct TequilaConfig {
+    organization: String,
+    server: String,
+    domain: String,
+    manager: String,
+    cookies: String, // Could be a bool, but its not documented, so I'm not gonna take this risk
+    support_certificates: String, // Same as above
+    default_languagge: String,
+    attributes: Vec<String>,
+    certificate: String,
+}
+
+#[derive(Debug)]
+pub enum ConfigError {
+    MissingEntry(String),
+    Request(reqwest::Error),
+    Url(url::ParseError)
+}
+
+impl TequilaConfig {
+    fn from_string(s: String) -> Result<Self, ConfigError> {
+        fn extract_value(lines: &[&str], name: &str) -> Result<String, ConfigError> {
+            match lines
+                .iter()
+                .find_map(|l| l.strip_prefix(format!("{name}:").as_str()))
+            {
+                Some(v) => Ok(v.trim().to_string()),
+                None => Err(ConfigError::MissingEntry(name.into())),
+            }
+        }
+
+        let lines = s.split("\n").collect::<Vec<_>>();
+        Ok(TequilaConfig {
+            organization: extract_value(&lines, "Organization")?,
+            server: extract_value(&lines, "Server")?,
+            domain: extract_value(&lines, "Domain")?,
+            manager: extract_value(&lines, "Manager")?,
+            cookies: extract_value(&lines, "Cookies")?,
+            support_certificates: extract_value(&lines, "Support certificates")?,
+            default_languagge: extract_value(&lines, "Default language")?,
+            attributes: extract_value(&lines, "Supported user attributes ")? // Somehow there is an extra space before the colon
+                .split(" ")
+                .map(String::from)
+                .collect(),
+            certificate: extract_value(&lines, "Server certificate")?,
+        })
+    }
+
+    pub fn fetch(url: String) -> Result<Self, ConfigError> {
+        Self::from_string(
+            reqwest::blocking::get(Url::from_str(url.as_str()).map_err(ConfigError::Url)?.join("/getconfig").map_err(ConfigError::Url)?)
+                .map_err(ConfigError::Request)?
+                .text()
+                .map_err(ConfigError::Request)?,
+        )
+    }
+}
